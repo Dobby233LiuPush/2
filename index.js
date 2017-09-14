6ef76cd3f33fee00481dae6ac9e989bf5dababf7
Liu-Wen-Yuan <15816141883@163.com>
2017-09-13 22:28:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function main()
 Rep = httpGet()
     Document.write("Minecraft.net is: " + Rep.minecraft.net + "\n")
     Document.write("Minecraft session is: " + Rep.session.minecraft.net)
-    
+    alert("it is beta")
 }
 
 function httpGet()
